ea81d9976e2d866fbe0fbad760388d6796493793
Mohamed-Taher1 <mohamedtahermail@gmail.com>
2024-08-24 20:37:57 +0300

    new project added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2482344438"
+    serviceWorkerVersion: "815814254"
   }
 });
